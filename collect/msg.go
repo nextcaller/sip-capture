7c351123484d1d0af2259d0601da55ef30b4835e
Dan Root <dan@nextcaller.com>
2021-03-05 18:39:28 +0000

    Clean up commenting about CallID in collect.

--- collect/msg.go
+++ collect/msg.go
@@ -20,9 +20,18 @@ type Msg struct {
 // the SIP Call-ID (or i:) header if available, or a hash as of the entire SIP
 // message if not available.
 func NewMsg(sip *layers.SIP) *Msg {
-	cid := sip.GetCallID()
 	msg := append(sip.LayerContents(), sip.Payload()...)
+	cid := sip.GetCallID()
 	if cid == "" {
+		// This "shouldn't ever happen".  By RFC3261, CallID must be present
+		// and globally unique.  However, if we're on the downstream side of
+		// some internal process that runs over SIP messages, we might see a
+		// message whose CallID is missing or unextractable.  In which case,
+		// we'll make an ID that's dependent on the contents of the message (so
+		// it's unique) using a hash.  This is not sufficient in the face of a
+		// determined malicious attacker with control over SIP inputs but is
+		// fast and will suffice for use cases where internal SIP messages
+		// might be missing a CallID.
 		h := fnv.New64a()
 		_, _ = h.Write(msg)
 		cid = fmt.Sprintf("%x", h.Sum64())
